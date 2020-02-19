e202b8972d3289f72c88a091e6a007c64f2c3275
Adam Vandolder <adam.vandolder@gmail.com>
2020-02-19 01:10:23 -0500

    Better failure message

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use chip8;
 
 fn main() -> Result<(), chip8::Error> {
-    let mut emu = chip8::Emulator::new().expect("Emulator failed to initialize.");
+    let mut emu = chip8::Emulator::new().expect("Failed to create emulator.");
     emu.run()
 }
